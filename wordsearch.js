c9916287bddd9a9ba8b41be524fa2ad16adc8491
Antonina Chichikalova <tonia.chichikalova@gmail.com>
2021-09-22 19:08:54 +0000

    Add wordSearch.js file

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,16 @@
 const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+  const horizontalJoin = letters.map(ls => ls.join(''));
+  for (l of horizontalJoin) {
+    if (l.includes(word)) return true;
+  }
+  let verticalJoin = '';
+  for (let i = 0; i < letters[0].length; i++) {
+    for (let j = 0; j < letters.length; j++) {     
+      verticalJoin = verticalJoin + letters[j][i];  
     }
-}
+  }
+  if (verticalJoin.includes(word)) return true;
+  return false;
+};
 
-module.exports = wordSearch
+module.exports = wordSearch;
